e2a236e15ff37f70fe56164dc6f61c7b6969e258
mig <michaelleojacob@gmail.com>
2022-04-24 14:17:25 -0700

    doing a commit so the css issue with the nav-drop-down is live so that others may attempt to help me with positioning the nav drop down menu

--- src/app/components/nav.js
+++ src/app/components/nav.js
@@ -1,23 +1,32 @@
+import NavDropdown from "./navdropdown";
+import { useState } from "react";
+
 const Nav = (props) => {
   const { endGame, characters } = props;
+  const [dropIsOpen, setDropIsOpen] = useState(false);
 
-  const checkData = () => console.log(characters);
+  // this is for testing purposes only
+  const handleCheckData = () => console.log(characters);
 
-  const handleDisplayCharacters = () => {
-    console.log("this was clicked");
-  };
+  const toggleIsOpen = () => setDropIsOpen(!dropIsOpen);
+
+  const handleDropdown = () => toggleIsOpen();
 
   return (
     <div id="nav-container">
       <button onClick={endGame}>end game</button>
       <div>00.00.00</div>
-      <button onClick={checkData}>dataCheck</button>
-      <button onClick={handleDisplayCharacters}>
-        {
-          Object.values(characters).filter((char) => char.found === false)
-            .length
-        }
-      </button>
+      <button onClick={handleCheckData}>dataCheck</button>
+      <div id="dropdown-container">
+        <button id="dropbtn" onClick={handleDropdown}>
+          {
+            Object.values(characters).filter((char) => char.found === false)
+              .length
+          }
+        </button>
+
+        {dropIsOpen ? <NavDropdown /> : null}
+      </div>
     </div>
   );
 };
---
+++ src/app/components/navdropdown.js
@@ -0,0 +1,11 @@
+const NavDropdown = () => {
+  return (
+    <div id="dropdown-content">
+      <div>thing1</div>
+      <div>thing2</div>
+      <div>thing3</div>
+    </div>
+  );
+};
+
+export default NavDropdown;
